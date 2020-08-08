d5b36e0756c2cb711fcde9d37d9b4150a8c2c148
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-08-07 21:37:32 -0300

    Continuação aula 3?

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
@@ -9,8 +9,17 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
 export default function Landing() {
+   const [totalConnections, setTotalConnections] = useState(0);
+
+   useEffect(() => {
+      api.get('connections').then(response => {
+         setTotalConnections(response.data.total);
+      })
+   }, []);
+
    return (
       <div id="page-landing">
          <div id="page-landing-content">
@@ -19,9 +28,9 @@ export default function Landing() {
                <h2>Sua plataforma de estudos online</h2>
             </div>
 
-            <img 
-               src={landingImg} 
-               alt="Plataforma de estudos" 
+            <img
+               src={landingImg}
+               alt="Plataforma de estudos"
                className="hero-image"
             />
 
@@ -37,7 +46,7 @@ export default function Landing() {
                </Link>
             </div>
             <span className="total-connections">
-               Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"></img>
+               Total de { totalConnections } conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"></img>
             </span>
          </div>
       </div>

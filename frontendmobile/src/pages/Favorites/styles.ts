1af7d9050c1544878bf987d51c671253da064cd7
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-08-09 11:03:07 -0300

    Finalização da semana

---
+++ frontendmobile/src/pages/Favorites/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1, // Para que ocupe toda a tela
+      backgroundColor: '#f0f0f7',
+   },
+   
+   teacherList: {
+      marginTop: -40,
+   },
+});
+
+export default styles;
